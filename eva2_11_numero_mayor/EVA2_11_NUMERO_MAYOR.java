abf006b11a9b71dace52206bb7d845af4d10ef0f
enriquejordan2 <L24550342@chihuahua2.tecnm.mx>
2024-11-02 10:57:20 -0600

    Add files via upload

---
+++ eva2_11_numero_mayor/EVA2_11_NUMERO_MAYOR.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_11_numero_mayor;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jose Enrique
+ */
+public class EVA2_11_NUMERO_MAYOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num1, acum;
+       
+        acum=0;
+        do{
+          Scanner captu = new Scanner(System.in);
+        System.out.println("Introdusca el numero deseado: ");
+        num1 = captu.nextInt();
+        acum=num1 ;
+        }while(num1 >=0);
+        //System.out.println("el resultado mayor es: "+ acum); 
+         else if{
+         (acum >=0)
+            System.out.println("el resultado mayor es: "+ acum);
+     
+        }else 
+                 
+}
